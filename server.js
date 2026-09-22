8fd34c6a43675c83d95e115d7e4ef2c5952834ea
Чергизов Абдул-Малик И <student399@ithub.local>
2026-09-22 10:19:24 +0300

    first commit

--- server.js
+++ server.js
@@ -8,15 +8,32 @@ app.get('/' , (req , res) => {
     res.send("Главная страница")
 })
 
-app.get('/abaut' , (req , res) => {
-    res.send("Abaut")
+app.get('/about' , (req , res) => {
+    res.send("About")
 })
 
-// const server = http.createServer((req, res)=>{
-//     res.end("Hello World")
+app.get('/contacts' , (req , res) => {
+    res.send("Контакты")
+})
+
+app.get('/products/:id' , (req, res)=>{
+    console.log(req.params.id);
+    
+    res.send(`Продукт id: ${req.params.id}`)
+})
+app.get('/number/:id' , (req, res)=>{
+    console.log(req.params.id);
+    
+    res.send(`Ваш номер телефона ${req.params.id}`)
+})
+
+app.post('/addproduct' , (req , res)=>{
+    const {name , age} = req.body;
+})
 
 
-// })
+
+
 
 app.listen(PORT, ()=>{
     console.log("Сервер запущен успешно по адресу: 127.0.0.1:3000");
